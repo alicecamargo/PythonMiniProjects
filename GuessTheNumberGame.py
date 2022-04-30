ea7b1d3ddfe89ca2ff8eee84967fd7f73ab62dd3
Alice <88071202+lilice523@users.noreply.github.com>
2022-04-30 19:30:35 -0300

    Add files via upload

---
+++ GuessTheNumberGame.py
@@ -0,0 +1,36 @@
+import random
+
+top_range_number = input("Type a number: ")
+
+if top_range_number.isdigit():
+    top_range_number = int(top_range_number)
+
+    if top_range_number <= 0:
+        print("Please type a number larger than 0 next time.")
+        quit()
+else:
+    print("Please type a number next time.")
+    quit()
+
+random_number = random.randint(0, top_range_number)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")
+        quit()
+        continue
+
+    if user_guess == random_number:
+        print("You got it!")
+        break
+    elif user_guess > random_number:
+        print("You were above the number!")
+    else:
+        print("You were below the number!")
+
+print("You got it in", guesses, "guesses")
